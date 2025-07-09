cf56970b4a4cf96472df975d89ab2de708c55df6
Henry <codinghen@gmail.com>
2025-07-09 17:44:41 +0100

    fix: move setInitialTheme call after imports to fix ESLint build error

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
-import { setInitialTheme } from "./theme";
-setInitialTheme();
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import { setInitialTheme } from './theme';
 
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
+setInitialTheme(); // 👈 call AFTER imports, before render
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
