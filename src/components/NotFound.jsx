cf6e3da874e92c3cf86b72229c7224fb766ed241
Henry <codinghen@gmail.com>
2025-07-15 14:56:07 +0100

    feat(frontend): prevent indexing of 404 page

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -1,17 +1,21 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Helmet } from 'react-helmet';
 
 export default function NotFound() {
   return (
-    <div className="flex flex-col items-center justify-center h-screen text-center p-8">
-      <h1 className="text-6xl font-extrabold">404</h1>
-      <p className="mt-4 text-lg">Sorry, we couldn’t find that page.</p>
-      <Link
-        to="/"
-        className="mt-6 text-blue-600 hover:underline"
-      >
-        Go back home
-      </Link>
-    </div>
+    <>
+      <Helmet>
+        <meta name="robots" content="noindex, noarchive" />
+      </Helmet>
+
+      <div className="flex flex-col items-center justify-center h-screen text-center p-8">
+        <h1 className="text-6xl font-extrabold">404</h1>
+        <p className="mt-4 text-lg">Sorry, we couldn’t find that page.</p>
+        <Link to="/" className="mt-6 text-blue-600 hover:underline">
+          Go back home
+        </Link>
+      </div>
+    </>
   );
 }
